f7db7e28e5a69279ebdeaeff68b2dc745c1b10a5
Anuoluwapo <63651250+Owoade@users.noreply.github.com>
2022-10-18 19:54:36 -0700

    Implemented cache layer for a rapidAPI servive

--- index.ts
+++ index.ts
@@ -16,22 +16,18 @@ const redis = createClient({
 
 redis.on("error", err => console.log(err) )
 
-app.get("/", async function( req: Request, res: Response){
-   const user = await redis.get("user");
-   console.log(user);
-   res.send(user)
-})
 
 app.get("/currency/:currencyCode", async function( req: Request, res: Response){
     const { currencyCode } = req.params;
+
     redis.get(currencyCode)
-    .then( async (rates)=>{
-        if(rates == null){
+    .then( async ( rates ) => {
+        if( rates == null ){
             console.log("Cache miss");
             // Get data from api end point
             const API_KEY = process.env.RAPID_API_KEY;
-            const ax_res = await axios.get(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${currencyCode}`);
-            res.json(ax_res.data.conversion_rates);
+            const ax_res = await axios.get( `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${currencyCode}` );
+            res.json( ax_res.data.conversion_rates );
             // Error Handling
             if( ax_res.data.result === "success" ){
                 const conversion_rates = JSON.stringify(ax_res.data.conversion_rates)
